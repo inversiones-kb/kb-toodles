ad137b7df4c6768dd595ee34537a8365f32e5a81
inversiones-kb <elcincomilazodelafria@gmail.com>
2026-07-25 17:46:02 -0400

    fixing loading spiner in notes

--- src/app/[branch]/dashboard/notas/page.tsx
+++ src/app/[branch]/dashboard/notas/page.tsx
@@ -166,13 +166,14 @@ export default function NotesPage() {
         </Modal>
 
         <CardTitle Icon={IconNote} title="Notas" />
-        <div className="w-full overflow-y-auto h-full columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-4">
-          {isLoading ? (
-            <div className="flex justify-center w-full">
-              <Spinner label="Cargando notas..." />
-            </div>
-          ) : null}
 
+        {isLoading ? (
+          <div className="flex justify-center w-full h-fit">
+            <Spinner label="Cargando notas..." />
+          </div>
+        ) : null}
+
+        <div className="w-full overflow-y-auto h-full columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-4">
           {!isLoading && data ? (
             data.length ? (
               data.map((note) => {
